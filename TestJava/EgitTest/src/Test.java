ff521a9c6dc7faca98fd7832656271832be76537
Johannes Ucel <u.johannes@hotmail.com>
2013-09-10 19:55:31 +0200

    EGit is Shitty

---
+++ TestJava/EgitTest/src/Test.java
@@ -0,0 +1,7 @@
+
+public class Test {
+public static void main(String[]args){
+	System.out.println("Hello World, Hello Stöger. Mit EGit wird das automatisch gepusht. Ist ein bisschen angenehmer. Einfach auf das Projekt, Rechte Maustatste, Team - EGit und fertig!");
+	System.out.println("By By World");
+	}
+}
